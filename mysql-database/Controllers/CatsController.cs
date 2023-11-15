7187657f509f3e7c4873c3cda65319bd0089b4c2
Daria Łuniewska <luniewska.d@gmail.com>
2023-11-15 22:14:13 +0100

    scaffolded new controller for cats, and also PUT in database has problems. It probably confirms my suspicions about DateTime

---
+++ mysql-database/Controllers/CatsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mysql_database.Data;
+using mysql_database.Models;
+
+namespace mysql_database.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatsController : ControllerBase
+    {
+        private readonly CatsContext _context;
+
+        public CatsController(CatsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cat>>> GetCats()
+        {
+          if (_context.Cats == null)
+          {
+              return NotFound();
+          }
+            return await _context.Cats.ToListAsync();
+        }
+
+        // GET: api/Cats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cat>> GetCat(int id)
+        {
+          if (_context.Cats == null)
+          {
+              return NotFound();
+          }
+            var cat = await _context.Cats.FindAsync(id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return cat;
+        }
+
+        // PUT: api/Cats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCat(int id, Cat cat)
+        {
+            if (id != cat.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cat>> PostCat(Cat cat)
+        {
+          if (_context.Cats == null)
+          {
+              return Problem("Entity set 'CatsContext.Cats'  is null.");
+          }
+            _context.Cats.Add(cat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCat", new { id = cat.Id }, cat);
+        }
+
+        // DELETE: api/Cats/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCat(int id)
+        {
+            if (_context.Cats == null)
+            {
+                return NotFound();
+            }
+            var cat = await _context.Cats.FindAsync(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cats.Remove(cat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CatExists(int id)
+        {
+            return (_context.Cats?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
