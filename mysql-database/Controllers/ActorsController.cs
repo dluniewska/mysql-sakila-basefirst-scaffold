6dae10be6b6cca5cf3900c39d82c18c48b0a114a
Daria Łuniewska <luniewska.d@gmail.com>
2023-11-15 19:15:35 +0100

    Initial for test project with scafolding mySql built-in sakila database

---
+++ mysql-database/Controllers/ActorsController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mysql_database.sakila;
+
+namespace mysql_database.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private readonly SakilaContext _context;
+
+        public ActorsController(SakilaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Actors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Actor>>> GetActors()
+        {
+          if (_context.Actors == null)
+          {
+              return NotFound();
+          }
+            return await _context.Actors.ToListAsync();
+        }
+
+        // GET: api/Actors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Actor>> GetActor(ushort id)
+        {
+          if (_context.Actors == null)
+          {
+              return NotFound();
+          }
+            var actor = await _context.Actors.FindAsync(id);
+
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return actor;
+        }
+
+        // PUT: api/Actors/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutActor(ushort id, Actor actor)
+        {
+            if (id != actor.ActorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(actor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Actors
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Actor>> PostActor(Actor actor)
+        {
+          if (_context.Actors == null)
+          {
+              return Problem("Entity set 'SakilaContext.Actors'  is null.");
+          }
+            _context.Actors.Add(actor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetActor", new { id = actor.ActorId }, actor);
+        }
+
+        // DELETE: api/Actors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActor(ushort id)
+        {
+            if (_context.Actors == null)
+            {
+                return NotFound();
+            }
+            var actor = await _context.Actors.FindAsync(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Actors.Remove(actor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ActorExists(ushort id)
+        {
+            return (_context.Actors?.Any(e => e.ActorId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ mysql-database/sakila/SalesByStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mysql_database.sakila;
+
+public partial class SalesByStore
+{
+    public string? Store { get; set; }
+
+    public string? Manager { get; set; }
+
+    public decimal? TotalSales { get; set; }
+}
